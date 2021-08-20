e407bed5970dd4ce15ad1e2ee91031d64f7ecb4f
Grzegorz Dziubak <grzegorzdziubak@gmail.com>
2021-08-20 12:11:32 +0200

    Add code for btns and addition

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -6,10 +6,122 @@ MainWindow::MainWindow(QWidget *parent)
    , ui(new Ui::MainWindow)
 {
    ui->setupUi(this);
+
+   init();
 }
 
 MainWindow::~MainWindow()
 {
    delete ui;
 }
+
+void MainWindow::init()
+{
+   stringValue = nullptr;
+   actualValue = 0.0;
+   newValue = 0.0;
+   oldValue = 0.0;
+}
+
+double MainWindow::getLcdValue()
+{
+   return ui->lcdNumber->value();
+}
+
+void MainWindow::setLcdValue(QString &val)
+{
+   ui->lcdNumber->display(val);
+   this->stringValue = QString::number(getLcdValue());
+   this->actualValue = getLcdValue();
+}
+
+void MainWindow::setLcdValue(double &val)
+{
+   ui->lcdNumber->display(val);
+
+}
+
+
+void MainWindow::on_btn_addition_clicked()
+{
+   if (oldValue == 0)
+   {
+      this->oldValue = actualValue;
+   }
+   else
+   {
+      this->newValue = oldValue + actualValue;
+      setLcdValue(newValue);
+   }
+}
+
+void MainWindow::on_btn_coma_clicked()
+{
+   stringValue += ",";
+   setLcdValue(stringValue);
+}
+
+void MainWindow::on_btn_0_clicked()
+{
+   stringValue += "0";
+   setLcdValue(stringValue);
+}
+
+void MainWindow::on_btn_1_clicked()
+{
+   stringValue += "1";
+   setLcdValue(stringValue);
+}
+
+void MainWindow::on_btn_2_clicked()
+{
+   stringValue += "2";
+   setLcdValue(stringValue);
+}
+
+void MainWindow::on_btn_3_clicked()
+{
+   stringValue += "3";
+   setLcdValue(stringValue);
+}
+
+void MainWindow::on_btn_4_clicked()
+{
+   stringValue += "4";
+   setLcdValue(stringValue);
+}
+
+void MainWindow::on_btn_5_clicked()
+{
+   stringValue += "5";
+   setLcdValue(stringValue);
+}
+
+void MainWindow::on_btn_6_clicked()
+{
+   stringValue += "6";
+   setLcdValue(stringValue);
+}
+
+void MainWindow::on_btn_7_clicked()
+{
+   stringValue += "7";
+   setLcdValue(stringValue);
+}
+
+void MainWindow::on_btn_8_clicked()
+{
+   stringValue += "8";
+   setLcdValue(stringValue);
+}
+
+void MainWindow::on_btn_9_clicked()
+{
+   stringValue += "9";
+   setLcdValue(stringValue);
+}
+
+
+
+
 
--- MainWindow.h
+++ MainWindow.h
@@ -15,7 +15,36 @@ public:
    MainWindow(QWidget *parent = nullptr);
    ~MainWindow();
 
+private slots:
+   void on_btn_0_clicked();
+   void on_btn_1_clicked();
+   void on_btn_2_clicked();
+   void on_btn_3_clicked();
+   void on_btn_4_clicked();
+   void on_btn_5_clicked();
+   void on_btn_6_clicked();
+   void on_btn_7_clicked();
+   void on_btn_8_clicked();
+   void on_btn_9_clicked();
+   void on_btn_coma_clicked();
+
+   void on_btn_addition_clicked();
+
+
+
 private:
    Ui::MainWindow *ui;
+
+   void init();
+   QString stringValue;
+
+   double actualValue;
+   double newValue;
+   double oldValue;
+
+   double getLcdValue();
+   void setLcdValue(QString &val);
+   void setLcdValue(double &val);
+
 };
 #endif // MAINWINDOW_H
